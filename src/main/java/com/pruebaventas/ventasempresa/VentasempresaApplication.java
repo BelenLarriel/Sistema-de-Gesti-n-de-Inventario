c99d4da8000323dba8fba67fe9de75cd4f6eebef
BelenLarriel <larriellucia@gmail.com>
2026-04-18 12:27:45 -0300

    primer commit

---
+++ src/main/java/com/pruebaventas/ventasempresa/VentasempresaApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaventas.ventasempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasempresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasempresaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebaventas/ventasempresa/VentasempresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaventas.ventasempresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasempresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
